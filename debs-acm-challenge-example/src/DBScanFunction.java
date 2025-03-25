bea3a1490f8e159f50cdf609ecef977bdd76773c
Tanish Bhowmick <tanishbhowmick@gmail.com>
2025-03-25 01:51:08 -0400

    fixed outlier detection logic to account for depth and adjusted dbscan clustering parameters

--- debs-acm-challenge-example/src/DBScanFunction.java
+++ debs-acm-challenge-example/src/DBScanFunction.java
@@ -12,11 +12,11 @@ public class DBScanFunction implements MapFunction<Tuple2<JSONObject, List<Outli
         List<OutlierDetectionFunction.OutlierPoint> outliers = in.f1;
         batch.put("outlier_count", outliers.size());
 
-        // Parameters for DBScan.
-        double eps = 2.0;
-        int minPts = 3;
+        // Adjusted parameters to match Python's DBSCAN.
+        double eps = 20.0;
+        int minPts = 5;
 
-        // Perform DBScan clustering.
+        // Perform DBSCAN clustering in 2D (using row and col).
         List<DBScanCluster> clusters = dbScan(outliers, eps, minPts);
 
         // Use a minheap to keep track of the top 10 clusters by size.
@@ -32,7 +32,7 @@ public class DBScanFunction implements MapFunction<Tuple2<JSONObject, List<Outli
         List<DBScanCluster> topClusterList = new ArrayList<>(topClusters);
         topClusterList.sort((a, b) -> Integer.compare(b.points.size(), a.points.size()));
 
-        // Compute centroids for the top clusters.
+        // Compute centroids for each top cluster.
         JSONArray centroidsArray = new JSONArray();
         for (DBScanCluster cluster : topClusterList) {
             double sumRow = 0;
@@ -49,7 +49,6 @@ public class DBScanFunction implements MapFunction<Tuple2<JSONObject, List<Outli
             centroidObj.put("y", centroidCol);
             centroidObj.put("count", cluster.points.size());
             centroidsArray.put(centroidObj);
-            //System.out.println("Cluster " + cluster.clusterId + ": Centroid (" + centroidRow + ", " + centroidCol + "), Size " + cluster.points.size());
         }
 
         batch.put("centroids", centroidsArray);
